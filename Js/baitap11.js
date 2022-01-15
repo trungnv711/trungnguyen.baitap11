b63919a69a6da4652e897e4db8a0964fee437257
Your Name <you@example.com>
2022-01-15 10:59:07 +0700

    baitap11

---
+++ Js/baitap11.js
@@ -0,0 +1,10 @@
+//bai tap 2
+let i = 0;
+function buttonClick() {
+    document.getElementById("inc").value = ++i;
+};
+
+//bai tap 1
+function buttonClickcolor (){
+    document.body.style.backgroundColor = 'rgb(225,24,233)';
+}
